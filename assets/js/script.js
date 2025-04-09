23348ae0c8b5773c95192d40c1a929f91b7c318d
Robert Ducusin <robertvictor.ducusin@icloud.com>
2025-04-09 22:20:45 +0800

    updated files

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function ($) {
+  const oneFourthScreen = $(window).height() / 4;
+  const halfScreen = $(window).height() / 2 + 100;
+  const header = $(".default-header");
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > oneFourthScreen) {
+      header.addClass("fixed-header");
+      if ($(window).scrollTop() > halfScreen) {
+        header.removeClass("hide-header");
+        header.addClass("show-header");
+      } else {
+        header.addClass("hide-header");
+        header.removeClass("show-header");
+      }
+    } else {
+      header.removeClass("fixed-header");
+    }
+
+    jQuery(".skill-progress").each(function () {
+      if (!jQuery(this).hasClass("animate-bar")) {
+        if (
+          jQuery(window).scrollTop() + jQuery(window).height() >
+          jQuery(this).offset().top
+        ) {
+          jQuery(this).addClass("animate-bar");
+        } else {
+          jQuery(this).removeClass("animate-bar");
+        }
+      }
+    });
+  });
+});
