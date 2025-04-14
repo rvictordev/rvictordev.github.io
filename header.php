2cdb424ba1faa83cd8aaefe7e7c554d747a4c18a
Robert Ducusin <rvducusindevs@gmail.com>
2025-04-14 13:56:16 +0800

    file updates

---
+++ header.php
@@ -0,0 +1,25 @@
+<header class="default-header hide-header">
+  <div class="container">
+    <div class="row justify-spacebetween">
+      <div class="left-row">
+        <div class="site-logo">
+          <a href="/">
+            <span>Robert Victor Ducusin</span>
+          </a>
+        </div>
+      </div>
+      <div class="right-row">
+        <nav class="menu-header-item">
+          <ul>
+            <li class="menu-item">
+              <a href="#about-me">About</a>
+            </li>
+            <li class="menu-item">
+              <a href="#work">Work</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</header>
