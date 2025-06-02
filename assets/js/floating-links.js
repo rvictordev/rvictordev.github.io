dbe5b5d9f73f285b2363c07e2bf656e88af2a3e3
Robert Ducusin <rvducusindevs@gmail.com>
2025-06-02 13:37:20 +0800

    updated files

--- assets/js/floating-links.js
+++ assets/js/floating-links.js
@@ -1,12 +1,24 @@
 jQuery(document).ready(function ($) {
   $(window).scroll(function () {
     if ($(window).scrollTop() > $(window).height()) {
-      $(".floating-links a").each(function (i) {
-        const link = $(this);
-        setTimeout(function () {
-          link.removeClass("hide-links");
-        }, 100 * i);
-      });
+      if (
+        $(window).scrollTop() >
+        $("#footer").offset().top - $(window).height()
+      ) {
+        $(".floating-links a").each(function (i) {
+          const link = $(this);
+          setTimeout(function () {
+            link.addClass("hide-links");
+          }, 100 * i);
+        });
+      } else {
+        $(".floating-links a").each(function (i) {
+          const link = $(this);
+          setTimeout(function () {
+            link.removeClass("hide-links");
+          }, 100 * i);
+        });
+      }
     } else {
       $(".floating-links a").each(function (i) {
         const link = $(this);
